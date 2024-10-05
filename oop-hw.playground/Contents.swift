0679755ffe18573cb959768d70b0daf554f61b96
Jake Smolowe <jakesmolowe@gmail.com>
2024-10-05 09:51:23 -0700

    Exercise 1 complete

--- oop-hw.playground/Contents.swift
+++ oop-hw.playground/Contents.swift
@@ -1,3 +1,44 @@
-import UIKit
+import SwiftUI
 
-var greeting = "Hello, playground"
+// Exercise 1
+
+class StringManager {
+  class func toLocalizedValue(num: Int) -> String {
+    let formatter = NumberFormatter()
+    formatter.locale = Locale(identifier: "en-US")
+    let localizedLikes = formatter.string(from: NSNumber(integerLiteral: num))!
+    return localizedLikes
+  }
+}
+
+class Post {
+  let author: String
+  let content: String
+  let likes: Int
+  
+  init(author: String, content: String, likes: Int) {
+    self.author = author
+    self.content = content
+    self.likes = likes
+  }
+  
+  func display() {
+    let formattedLikes = StringManager.toLocalizedValue(num: likes)
+    let post = """
+    \(author)
+    
+    \(content)
+
+    Likes: \(formattedLikes)
+    """
+    print(post)
+  }
+}
+
+let post1 = Post(author: "Jake Smolowe", content: "What is everybody listening to today? I'm listening to one of the baddest pianists nobody talks about, Clare Fischer.", likes: 78352)
+
+let post2 = Post(author: "Luke Cat Amata", content: "Anybody else have an owner that refuses to let them jump on the table and destroy documents whenever they want? This is opression!", likes: 315279)
+
+post1.display()
+print("---------")
+post2.display()
